90ebb2ea52d4805fbeac3c4cb79069567059c690
rosmijarvis <rosmi.poonath@lntinfotech.com>
2016-11-11 11:03:13 +0530

    Add files via upload

---
+++ channels/webchat/ui/server_01.js
@@ -0,0 +1,43 @@
+'use strict';
+
+var PropertiesReader = require('properties-reader');
+var express = require('express');
+var app = express();
+var https = require('https');
+var fs = require('fs');
+var sharedProperties = PropertiesReader("../webroot/channels/webchat/config/shared-properties.js");
+
+
+class UIServer
+{
+	constructor(mode)
+	{   
+		app.use(express.static('public'));
+		
+		if ('secure' == mode)
+		{
+			let webAppsPort=sharedProperties.get('appServer.uiSocketPort');
+			let httpsServer = https.createServer({
+			key: fs.readFileSync('../webroot/channels/webchat/certs/key.pem'),
+			cert: fs.readFileSync('../webroot/channels/webchat/certs/cert.pem')
+			},app).listen(webAppsPort);
+			
+			var host = httpsServer.address().address
+			var port = httpsServer.address().port
+
+			console.log("UI component listening in secure mode at https://%s:%s", host, port)
+		}
+		else
+		{
+			var server = app.listen(sharedProperties.get('appServer.uiSocketPort') , function () {
+			var host = server.address().address
+			var port = server.address().port
+			console.log("UI component listening at http://%s:%s", host, port)
+			})
+		}
+			
+	}
+		
+} 
+module.exports.UIServer = UIServer;	
+		
