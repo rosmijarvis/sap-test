64be956ab39e77d13547050dcb73761413fc7b6a
rosmijarvis <rosmi.poonath@lntinfotech.com>
2016-11-11 11:01:31 +0530

    Add files via upload

---
+++ serverStudio.js
@@ -0,0 +1,29 @@
+'use strict';
+var UIServer = require('./channels/webchat/ui/server.js').UIServer;
+var option='';
+
+process.argv.forEach(function (val, index, array) 
+{	
+	
+	//console.log(array.length + ' '+ val + ' '+index);
+	
+	if(array.length != 3)
+	{
+		console.log("Please provide option arg1 ( mode - secure/non-secure)");
+		process.exit(1);
+		return;			
+	}
+
+	// Take the bot name as a parameter
+	
+	if(index === 2)
+	{
+		option = val;
+		
+	}
+});
+
+var uiServer =  new UIServer(option);
+
+//chatServer.manageChatServer(option);
+
