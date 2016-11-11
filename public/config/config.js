56daa727f88a847ebfb42a1b4a55c59ee78e77bc
rosmijarvis <rosmi.poonath@lntinfotech.com>
2016-11-11 14:09:12 +0530

    Add files via upload

---
+++ public/config/config.js
@@ -0,0 +1,5 @@
+gOptions = {
+  // This can have nested stuff, arrays, etc.
+  binaryServerUrl:'wss://infarszc90882.nmumarl.lntinfotech.com:8086',
+  chatServerTextUrl:'https://infarszc90882.nmumarl.lntinfotech.com:8084'
+}
