cb811d804cfe691aa8ca5b251ef2a7825307963a
rosmijarvis <rosmi.poonath@lntinfotech.com>
2016-11-11 11:52:59 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,27 +1,7 @@
 'use strict';
 var UIServer = require('./channels/webchat/ui/server_01.js').UIServer;
-var option='';
+var option='non-secure';
 
-process.argv.forEach(function (val, index, array) 
-{	
-	
-	//console.log(array.length + ' '+ val + ' '+index);
-	
-	if(array.length != 3)
-	{
-		console.log("Please provide option arg1 ( mode - secure/non-secure)");
-		process.exit(1);
-		return;			
-	}
-
-	// Take the bot name as a parameter
-	
-	if(index === 2)
-	{
-		option = val;
-		
-	}
-});
 
 var uiServer =  new UIServer(option);
 
